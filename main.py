37c26c2cbce8ee45316143e59391874e1ba222bc
dijac <darshanbhusal41@gmail.com>
2021-09-28 01:13:10 +0545

    detail_frame commit

--- main.py
+++ main.py
@@ -187,4 +187,17 @@ ShowAllbtn = Button(DataFrameRight, text="Show All", font=("arial", 12, "bold"),
                     command=query)
 ShowAllbtn.grid(row=9, column=0, padx=3, pady=3)
 
+# Details frame
+details_frame = Frame(root, bd=10, relief=RIDGE)
+details_frame.place(x=0, y=540, width=1279, height=300)
+
+# details and scrollbar
+sc_x = ttk.Scrollbar(details_frame, orient=HORIZONTAL)
+sc_x.pack(side=BOTTOM, fill=X)
+sc_y = ttk.Scrollbar(details_frame, orient=VERTICAL)
+sc_y.pack(side=RIGHT, fill=Y)
+
+sc_x.pack(side=BOTTOM, fill=X)
+sc_y.pack(side=RIGHT, fill=Y)
+
 mainloop()
