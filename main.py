1a8ac4d20c6c480ab573046e2f44fd9bf0bd0758
dijac <darshanbhusal41@gmail.com>
2021-09-28 01:07:22 +0545

    dataframe_left_image commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #imports
 from tkinter import *
-
+from PIL import Image,ImageTk
 from tkinter import ttk
 import sqlite3
 from tkinter import messagebox
@@ -38,5 +38,34 @@ root.geometry("1280x680")
 # top title
 title = Label(root, text="Pharmacy Management System", bg="black", fg="red", font=("Bradley Hand ITC", 50, "bold"))
 title.pack(side=TOP, fill=X, padx=10, pady=10)
+
+# logo image
+img1 = Image.open("logo.png")
+img1 = img1.resize((80, 80), Image.ANTIALIAS)
+photoimg1 = ImageTk.PhotoImage(img1)
+btn = Button(root, image=photoimg1, borderwidth=0)
+btn.place(x=40, y=14)
+
+# dataframe
+DataFrame = Frame(root, bd=10, bg="black", relief=RIDGE, padx=20, pady=20)
+DataFrame.place(x=0, y=110, width=1280, height=420)
+
+# dataframe left
+DataFrameLeft = LabelFrame(DataFrame, bd=10, bg="lightblue", relief=RIDGE, padx=20, text="Medicine Information",
+                           fg="black", font=("arial", 16, "bold"))
+DataFrameLeft.place(x=0, y=5, width=900, height=355)
+
+# images in left dataframe
+img2 = Image.open("tab.png")
+img2 = img2.resize((100, 80), Image.ANTIALIAS)
+photoimg2 = ImageTk.PhotoImage(img2)
+btn = Button(root, image=photoimg2, borderwidth=0)
+btn.place(x=800, y=177)
+
+img3 = Image.open("doc.png")
+img3 = img3.resize((430, 200), Image.ANTIALIAS)
+photoimg3 = ImageTk.PhotoImage(img3)
+btn = Button(root, image=photoimg3, borderwidth=0)
+btn.place(x=470, y=270)
 
 mainloop()
